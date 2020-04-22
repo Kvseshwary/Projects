67aef622a7f63939e3a9a78681c4d8130e5d4a71
Jerish PJ <jerishonline@gmail.com>
2020-04-22 23:08:20 +0100

    Put method added

--- WebApi/WebApi/App_Start/WebApiConfig.cs
+++ WebApi/WebApi/App_Start/WebApiConfig.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
-
+using Newtonsoft.Json.Serialization;
 namespace WebApi
 {
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
+            //Config is the object that points whetherJSON or XML data is coming in request /response
+
             // Web API configuration and services
 
             // Web API routes
@@ -19,6 +21,16 @@ namespace WebApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            //When a request is made to web api services the mediatype formatter class converts to Json 
+            //Media even if the accept header is in XMl Format..
+           // config.Formatters.Remove(config.Formatters.JsonFormatter);
+
+            //Work on the media type formatter
+
+           // config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+           // config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            
         }
     }
 }
